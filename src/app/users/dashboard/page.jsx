340a5318dc7b261b650ca0182c4c7b55aac4a701
Rhakelino <instanclay@gmail.com>
2025-10-22 21:38:40 +0700

    add navigation

--- src/app/users/dashboard/page.jsx
+++ src/app/users/dashboard/page.jsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import { AuthUserSession } from "@/app/libs/auth-libs";
 // PERBAIKAN 1: Impor 'redirect' dari 'next/navigation'
 import { redirect } from "next/navigation";
+import Navigation from "@/app/components/Navigation";
 
 async function DashboardPage() {
   const session = await AuthUserSession();
@@ -17,6 +18,7 @@ async function DashboardPage() {
   return (
   <section className="font-sans p-8 min-h-screen bg-gray-900 text-gray-100">
       
+      <Navigation href="/" text="Kembali ke Home"/>
       {/* Judul diubah ke text-white */}
       <h1 className="text-3xl font-bold text-white mb-6">
         Dashboard
